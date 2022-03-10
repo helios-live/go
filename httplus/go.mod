33d59543151c4dd84601d21a28097c9e94c8f587
Alexandru Eftimie - mac <eftimie.alexandru@gmail.com>
2022-03-10 05:46:54 +0200

    httplus

---
+++ httplus/go.mod
@@ -0,0 +1,3 @@
+module github.com/ideatocode/go/httplus
+
+go 1.17
---
+++ httplus/http.go
@@ -0,0 +1,36 @@
+package netutils
+
+import (
+	"net"
+	"net/http"
+	"strconv"
+)
+
+// GetHostPort returns host and port from a proxy HTTP request
+func GetHostPort(r *http.Request) (host string, port int) {
+	host = r.Host
+	host, sport, err := net.SplitHostPort(r.Host)
+	if err == nil {
+		port, _ = strconv.Atoi(sport)
+	} else {
+		if r.Host != "" {
+			host = r.Host
+		}
+		if r.URL.Scheme == "" || r.URL.Scheme == "http" {
+			port = 80
+		}
+		if r.URL.Scheme == "https" {
+			port = 443
+		}
+	}
+	return host, port
+}
+
+// CopyHeader copies all the headers from src to dst
+func CopyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
