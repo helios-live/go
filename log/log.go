5ec6f01a061d159cd9402f90945046d11b56f8ca
Alexandru Eftimie - mac <eftimie.alexandru@gmail.com>
2022-03-10 13:40:05 +0200

    always add space, fixed zerolog

--- log/log.go
+++ log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"fmt"
+	"io"
 
 	stdlog "log"
 
@@ -24,30 +25,36 @@ type Zero struct {
 	zerolog.Logger
 }
 
-func (z Zero) Debug(v ...interface{}) { z.Logger.Debug().Msg(fmt.Sprint(v...)) }
-func (z Zero) Info(v ...interface{})  { z.Logger.Info().Msg(fmt.Sprint(v...)) }
-func (z Zero) Log(v ...interface{})   { z.Logger.Log().Msg(fmt.Sprint(v...)) }
-func (z Zero) Warn(v ...interface{})  { z.Logger.Warn().Msg(fmt.Sprint(v...)) }
-func (z Zero) Error(v ...interface{}) { z.Logger.Error().Msg(fmt.Sprint(v...)) }
-func (z Zero) Fatal(v ...interface{}) { z.Logger.Fatal().Msg(fmt.Sprint(v...)) }
+// NewZero Returns a new Zerolog logger
+func NewZero(w io.Writer) *Zero {
+	return &Zero{
+		Logger: zerolog.New(w),
+	}
+}
+func (z Zero) Debug(v ...interface{}) { z.Logger.Debug().Msg(fmt.Sprintln(v...)) }
+func (z Zero) Info(v ...interface{})  { z.Logger.Info().Msg(fmt.Sprintln(v...)) }
+func (z Zero) Log(v ...interface{})   { z.Logger.Log().Msg(fmt.Sprintln(v...)) }
+func (z Zero) Warn(v ...interface{})  { z.Logger.Warn().Msg(fmt.Sprintln(v...)) }
+func (z Zero) Error(v ...interface{}) { z.Logger.Error().Msg(fmt.Sprintln(v...)) }
+func (z Zero) Fatal(v ...interface{}) { z.Logger.Fatal().Msg(fmt.Sprintln(v...)) }
 
 // Std is a basic re-implementation of the standard library log to match the logging interface
 type Std struct{}
 
-func (s Std) Debug(v ...interface{}) { stdlog.Print("[DEBUG]", fmt.Sprint(v...)) }
-func (s Std) Info(v ...interface{})  { stdlog.Print("[Info]", fmt.Sprint(v...)) }
-func (s Std) Log(v ...interface{})   { stdlog.Print("[Log]", fmt.Sprint(v...)) }
-func (s Std) Warn(v ...interface{})  { stdlog.Print("[Warn]", fmt.Sprint(v...)) }
-func (s Std) Error(v ...interface{}) { stdlog.Print("[Error]", fmt.Sprint(v...)) }
-func (s Std) Fatal(v ...interface{}) { stdlog.Print("[Fatal]", fmt.Sprint(v...)) }
+func (s Std) Debug(v ...interface{}) { stdlog.Print("[DEBUG]", fmt.Sprintln(v...)) }
+func (s Std) Info(v ...interface{})  { stdlog.Print("[Info]", fmt.Sprintln(v...)) }
+func (s Std) Log(v ...interface{})   { stdlog.Print("[Log]", fmt.Sprintln(v...)) }
+func (s Std) Warn(v ...interface{})  { stdlog.Print("[Warn]", fmt.Sprintln(v...)) }
+func (s Std) Error(v ...interface{}) { stdlog.Print("[Error]", fmt.Sprintln(v...)) }
+func (s Std) Fatal(v ...interface{}) { stdlog.Print("[Fatal]", fmt.Sprintln(v...)) }
 
 // Color is a basic re-implementation of the standard library log to match the logging interface
 // with added colors
 type Color struct{}
 
-func (Color) Debug(v ...interface{}) { stdlog.Print(color.GreenString("[DEBUG]"), fmt.Sprint(v...)) }
-func (Color) Info(v ...interface{})  { stdlog.Print(color.HiBlueString("[Info]"), fmt.Sprint(v...)) }
-func (Color) Log(v ...interface{})   { stdlog.Print(color.WhiteString("[Log]"), fmt.Sprint(v...)) }
-func (Color) Warn(v ...interface{})  { stdlog.Print(color.YellowString("[Warn]"), fmt.Sprint(v...)) }
-func (Color) Error(v ...interface{}) { stdlog.Print(color.RedString("[Error]"), fmt.Sprint(v...)) }
-func (Color) Fatal(v ...interface{}) { stdlog.Print(color.HiRedString("[Fatal]"), fmt.Sprint(v...)) }
+func (Color) Debug(v ...interface{}) { stdlog.Print(color.GreenString("[DEBUG]"), fmt.Sprintln(v...)) }
+func (Color) Info(v ...interface{})  { stdlog.Print(color.HiBlueString("[Info]"), fmt.Sprintln(v...)) }
+func (Color) Log(v ...interface{})   { stdlog.Print(color.WhiteString("[Log]"), fmt.Sprintln(v...)) }
+func (Color) Warn(v ...interface{})  { stdlog.Print(color.YellowString("[Warn]"), fmt.Sprintln(v...)) }
+func (Color) Error(v ...interface{}) { stdlog.Print(color.RedString("[Error]"), fmt.Sprintln(v...)) }
+func (Color) Fatal(v ...interface{}) { stdlog.Print(color.HiRedString("[Fatal]"), fmt.Sprintln(v...)) }
