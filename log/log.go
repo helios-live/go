614e118b2bf0533fca10ce0ea24a91339b14d26d
Alexandru Eftimie - mac <eftimie.alexandru@gmail.com>
2022-03-16 15:29:32 +0200

    changed Print to Println, exiting on fatal

--- log/log.go
+++ log/log.go
@@ -36,25 +36,30 @@ func (z Zero) Info(v ...interface{})  { z.Logger.Info().Msg(fmt.Sprintln(v...)) 
 func (z Zero) Log(v ...interface{})   { z.Logger.Log().Msg(fmt.Sprintln(v...)) }
 func (z Zero) Warn(v ...interface{})  { z.Logger.Warn().Msg(fmt.Sprintln(v...)) }
 func (z Zero) Error(v ...interface{}) { z.Logger.Error().Msg(fmt.Sprintln(v...)) }
-func (z Zero) Fatal(v ...interface{}) { z.Logger.Fatal().Msg(fmt.Sprintln(v...)) }
+func (z Zero) Fatal(v ...interface{}) { z.Logger.Fatal().Msg(fmt.Sprintln(v...)); stdlog.Fatal() }
 
 // Std is a basic re-implementation of the standard library log to match the logging interface
 type Std struct{}
 
-func (s Std) Debug(v ...interface{}) { stdlog.Print("[DEBUG]", fmt.Sprintln(v...)) }
-func (s Std) Info(v ...interface{})  { stdlog.Print("[Info]", fmt.Sprintln(v...)) }
-func (s Std) Log(v ...interface{})   { stdlog.Print("[Log]", fmt.Sprintln(v...)) }
-func (s Std) Warn(v ...interface{})  { stdlog.Print("[Warn]", fmt.Sprintln(v...)) }
-func (s Std) Error(v ...interface{}) { stdlog.Print("[Error]", fmt.Sprintln(v...)) }
-func (s Std) Fatal(v ...interface{}) { stdlog.Print("[Fatal]", fmt.Sprintln(v...)) }
+func (s Std) Debug(v ...interface{}) { stdlog.Println("[DEBUG]", fmt.Sprintln(v...)) }
+func (s Std) Info(v ...interface{})  { stdlog.Println("[Info]", fmt.Sprintln(v...)) }
+func (s Std) Log(v ...interface{})   { stdlog.Println("[Log]", fmt.Sprintln(v...)) }
+func (s Std) Warn(v ...interface{})  { stdlog.Println("[Warn]", fmt.Sprintln(v...)) }
+func (s Std) Error(v ...interface{}) { stdlog.Println("[Error]", fmt.Sprintln(v...)) }
+func (s Std) Fatal(v ...interface{}) { stdlog.Println("[Fatal]", fmt.Sprintln(v...)) }
 
 // Color is a basic re-implementation of the standard library log to match the logging interface
 // with added colors
 type Color struct{}
 
-func (Color) Debug(v ...interface{}) { stdlog.Print(color.GreenString("[DEBUG]"), fmt.Sprintln(v...)) }
-func (Color) Info(v ...interface{})  { stdlog.Print(color.HiBlueString("[Info]"), fmt.Sprintln(v...)) }
-func (Color) Log(v ...interface{})   { stdlog.Print(color.WhiteString("[Log]"), fmt.Sprintln(v...)) }
-func (Color) Warn(v ...interface{})  { stdlog.Print(color.YellowString("[Warn]"), fmt.Sprintln(v...)) }
-func (Color) Error(v ...interface{}) { stdlog.Print(color.RedString("[Error]"), fmt.Sprintln(v...)) }
-func (Color) Fatal(v ...interface{}) { stdlog.Print(color.HiRedString("[Fatal]"), fmt.Sprintln(v...)) }
+func (Color) Debug(v ...interface{}) {
+	stdlog.Println(color.GreenString("[DEBUG]"), fmt.Sprintln(v...))
+}
+func (Color) Info(v ...interface{})  { stdlog.Println(color.HiBlueString("[Info]"), fmt.Sprintln(v...)) }
+func (Color) Log(v ...interface{})   { stdlog.Println(color.WhiteString("[Log]"), fmt.Sprintln(v...)) }
+func (Color) Warn(v ...interface{})  { stdlog.Println(color.YellowString("[Warn]"), fmt.Sprintln(v...)) }
+func (Color) Error(v ...interface{}) { stdlog.Println(color.RedString("[Error]"), fmt.Sprintln(v...)) }
+func (Color) Fatal(v ...interface{}) {
+	stdlog.Println(color.HiRedString("[Fatal]"), fmt.Sprintln(v...))
+	stdlog.Fatal()
+}
